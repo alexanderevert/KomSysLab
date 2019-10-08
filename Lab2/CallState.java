4e23d23a2bff75d33cc4f40404a6c489bf56c640
simon <kampfsimon@gmail.com>
2019-10-08 10:24:36 +0200

    småfix

--- Lab2/CallState.java
+++ Lab2/CallState.java
@@ -4,55 +4,58 @@ import java.io.*;
 
 public abstract class CallState{
 
-  
 
-public boolean busy(){ 
+
+public boolean busy(){
   return false;
 }
 
-public CallState timedOut(){ 
-  error(); 
-  return new Free();
+public CallState timedOut(){
+  error();
+  return new CallStateFree();
 }
 
-public CallState userWantsToInvite(PrintWriter out){ 
-  error(); 
-  return new Free();
+public CallState userWantsToInvite(PrintWriter out){
+  error();
+  return new CallStateFree();
 }
 
 public void printState(){
 }
 
-public CallState receivedInvite(AudioStreamUDP audioStream, PrintWriter out){ 
-  error(); 
-  return new Free();
+public CallState receivedInvite(AudioStreamUDP audioStream, PrintWriter out){
+  error();
+  return new CallStateFree();
 }
 
-public CallState answerCall(InetAddress ip, int udpPort, AudioStreamUDP audioStream, PrintWriter out) { 
-  error(); 
-  return new Free();
+public CallState answerCall(InetAddress ip, int udpPort, AudioStreamUDP audioStream, PrintWriter out) {
+  error();
+  return new CallStateFree();
 }
 
 
-public CallState userWantsToQuit(AudioStreamUDP audioStream, PrintWriter out) { 
-  error(); 
-  return new Free();
+public CallState userWantsToQuit(AudioStreamUDP audioStream, PrintWriter out) {
+  error();
+  return new CallStateFree();
 }
-public CallState receivedBye(AudioStreamUDP audioStream, PrintWriter out) { 
-  error(); 
-  return new Free();
-}
-
-public CallState receivedOk(AudioStreamUDP audioStream) { 
-  error(); 
-  return new Free();
+public CallState receivedBye(AudioStreamUDP audioStream, PrintWriter out) {
+  error();
+  return new CallStateFree();
 }
 
-public CallState receivedAck(InetAddress ip, int udpPort, AudioStreamUDP audioStream) { 
-  error(); 
-  return new Free();
+public CallState receivedOk(AudioStreamUDP audioStream) {
+  error();
+  return new CallStateFree();
 }
 
-  
+public CallState receivedAck(InetAddress ip, int udpPort, AudioStreamUDP audioStream) {
+  error();
+  return new CallStateFree();
+}
+
+private void error(){
+  System.out.println("Error");
+}
+
 
 }
